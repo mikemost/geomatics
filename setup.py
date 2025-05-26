7df569ab45e0f3ed19506158706251b40e2c38c5
Michael Mutale <mikemost@gmail.com>
2025-05-26 11:42:25 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="geomatics",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "sympy",
+        "numpy",
+        "matplotlib"
+    ],
+    author="Michael Mutale",
+    author_email="mikemost@gmail.com",
+    description="A package for geomatics engineering calculations and utilities",
+    keywords="geomatics, surveying, engineering",
+    url="https://github.com/mikemost/geomatics",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
